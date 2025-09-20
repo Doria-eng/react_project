dfd99ebb0c6278234e39972dd7508cf237955cf2
Doria <silinren217@gmail.com>
2025-09-20 22:38:54 +0930

    update form implementation

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 function AppContent() {
-  const [text, setText] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username === "" || password === "") {
+      alert('"username" and "password" are required');
+      return;
+    }
+
+    alert("username", username);
+    alert("password", password);
+  }
+
+  // JSX syntax
   return (
     <main>
-      <input onChange={(event) => setText(event.target.value)} />
-      <p>{text}</p>
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </main>
   );
 }
